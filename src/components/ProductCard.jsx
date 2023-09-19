0218aec4714028da9dfacce4b42ffda8e61fddf9
milagrosvallejo <136761224+milagrosvallejo@users.noreply.github.com>
2023-09-19 14:59:41 -0300

    changes

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,10 @@
+import React from "react";
+
 function ProductCard() {
   return <div>ProductCard</div>;
+  
 }
 
+
 export default ProductCard;
+
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,8 +1,25 @@
-import { useContext } from "react";
+
+import { useContext, useState } from "react";
 import { ProductsContext } from "../context/ProductsContext";
+import ProductDescription from "../pages/ProductDescription";
 
 const ProductList = () => {
   const { products, isloading, error } = useContext(ProductsContext);
+  const [sortBy, setSortBy] = useState('');
+
+  const handleSort = (type) => {
+    // Función para cambiar el tipo de ordenamiento
+    setSortBy(type);
+  };
+
+  const sortedProducts = [...products]; // Clonamos el arreglo de productos para no modificarlo directamente
+
+  if (sortBy === 'ASC') {
+    sortedProducts.sort((a, b) => a.price - b.price); // Orden ascendente
+  } else if (sortBy === 'DESC') {
+    sortedProducts.sort((a, b) => b.price - a.price); // Orden descendente
+  }
+
   if (isloading) {
     return <div>Loading...</div>;
   }
@@ -12,9 +29,21 @@ const ProductList = () => {
   return (
     <>
       <h1>Product List</h1>
-      <ul>
+      <div className="mydiv">
+        <button className="my-button" onClick={() => handleSort('ASC')}>Precio ASC</button>
+        <button className="my-button" onClick={() => handleSort('DESC')}>Precio DESC</button>
+      </div>
+      {/* <ul>
         {products?.length &&
-          products.map((product) => <li key={product.id}>{product.title}</li>)}
+          products.map((product) => <li key={product.id}> <ProductDescription product={product} /> </li>)}
+      </ul> */}
+      <ul>
+        {sortedProducts.length > 0 &&
+          sortedProducts.map((product) => (
+            <li key={product.id}>
+              <ProductDescription product={product} />
+            </li>
+          ))}
       </ul>
     </>
   );
