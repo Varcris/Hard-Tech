dd052adc2d7c9b15686f66aa3f81446a7d57819d
Vargas Rondal Cristian Gustavo <cristianvargasplus@gmail.com>
2023-09-22 18:08:43 -0300

    envio al contexto searchText con un evento

--- src/context/ProductsContext.jsx
+++ src/context/ProductsContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState, useEffect } from "react";
 import { initialProduct } from "../services/initialProduct";
 import { getData } from "../services/api";
-import { endPoint } from "../services/endPoints";
+import { endpoint } from "../services/endPoints";
 
 export const ProductsContext = createContext(initialProduct); //
 
@@ -10,14 +10,23 @@ export const ProductsContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const [isloading, setIsLoading] = useState(true); // inicia en true porque cuando se carga la pagina se esta cargando la data
   const [error, setError] = useState(null);
+  const [searchText, setSearchText] = useState("");
+  const [limitPage, setLimitPage] = useState(15); // limite de productos por pagina
+  const [skip, setSkip] = useState(0); // cantidad de productos que se deben saltar para mostrar la pagina actual
+  const [currentPage, setCurrentPage] = useState(1); // pagina actual
 
-  const fetchProducts = async () => {
+  const fetchProducts = async (query) => {
+    console.log("FetchProducts en ejecución ---->");
+    let data = [];
     try {
       setError(null);
       setIsLoading(true);
-      const data = await getData(endPoint.getAllProducts);
-      console.log(data);
+      console.log("await getData en ejecución ---->");
+      data = await getData(query);
+      console.log("SetProducts en ejecución ---->");
       setProducts(data.products);
+      console.log("----> data");
+      console.log(data);
     } catch (error) {
       console.log(error);
       setError(error.message);
@@ -27,10 +36,32 @@ export const ProductsContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    fetchProducts();
-  }, []);
+    if (searchText === "") {
+      console.log("getAllProducts en ejecución ---->");
+      fetchProducts(endpoint.getAllProducts(limitPage, skip)); // se ejecuta cuando se carga la pagina por primera vez y cuando se cambia el valor de limitPage o de skip
+    } else {
+      console.log("searchProducts en ejecución ---->");
+      fetchProducts(endpoint.searchProducts(searchText)); // se ejecuta cuando se cambia el valor de searchText
+    }
+  }, [searchText, limitPage, skip]);
+
+  const handleChangeSearch = (searchText) => {
+    console.log("handleChangeSearch en ejecución");
+    setSearchText(searchText);
+    console.log(searchText);
+  };
   return (
-    <ProductsContext.Provider value={{ products, isloading, error }}>
+    <ProductsContext.Provider
+      value={{
+        products,
+        isloading,
+        error,
+        currentPage,
+        limitPage,
+        skip,
+        onChangeSearch: handleChangeSearch,
+      }}
+    >
       {children}
     </ProductsContext.Provider>
   );
