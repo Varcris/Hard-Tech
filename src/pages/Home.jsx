22ca7bfdba9bdd7fa393d2a99947d930694d5b6e
Vargas Rondal Cristian Gustavo <cristianvargasplus@gmail.com>
2023-12-09 15:27:46 -0300

    probando cosas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
 function Home() {
   return (
     <main>
-      <section>
-        <h2>Pagina Principal</h2>
-      </section>
-      
-      <div className="container">
-
-      </div>
+      <h1>Hola bienvenidos a Hardtech 🚀</h1>
+      <p>
+        Somos una empresa dedicada a la venta de productos de tecnología,
+        especialmente de hardware.
+      </p>
+      <p>
+        Nuestro objetivo es ofrecer productos de calidad a un precio accesible.
+      </p>
+      <p>
+        Contamos con un equipo de profesionales capacitados para asesorarte en
+        tu compra.
+      </p>
+      <p>Si tenés alguna duda o consulta no dudes en contactarnos.</p>
+      <p>¡Muchas gracias!</p>
     </main>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 export const getData = async (endpoint) => {
-  // const res = await fetch(`https://dummyjson.com/${endpoint}`);
   const res = await fetch(`http://localhost:3000/api/${endpoint}`);
   if (!res.ok) throw new Error("Error en la petición");
   const data = await res.json();
-  console.log(data);
+
   return data;
 };
